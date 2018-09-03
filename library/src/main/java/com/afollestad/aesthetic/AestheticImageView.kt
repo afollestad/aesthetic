5a2f52eb2427ff2261d9fb2ceef8fac3f70b90fa
Aidan Follestad <afollestad@squareup.com>
2018-09-03 16:16:27 -0700

    ImageView tinting support, resolves #91

--- library/src/main/java/com/afollestad/aesthetic/AestheticImageView.kt
+++ library/src/main/java/com/afollestad/aesthetic/AestheticImageView.kt
@@ -8,12 +8,14 @@ package com.afollestad.aesthetic
 import android.content.Context
 import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
+import com.afollestad.aesthetic.actions.ImageViewTintAction
 import com.afollestad.aesthetic.actions.ViewBackgroundAction
-import com.afollestad.aesthetic.utils.ViewUtil
+import com.afollestad.aesthetic.utils.ViewUtil.getObservableForResId
 import com.afollestad.aesthetic.utils.distinctToMainThread
 import com.afollestad.aesthetic.utils.onErrorLogAndRethrow
+import com.afollestad.aesthetic.utils.plusAssign
 import com.afollestad.aesthetic.utils.resId
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
 
 /** @author Aidan Follestad (afollestad) */
 class AestheticImageView(
@@ -21,30 +23,47 @@ class AestheticImageView(
   attrs: AttributeSet? = null
 ) : AppCompatImageView(context, attrs) {
 
-  private var bgSubscription: Disposable? = null
+  private var subs: CompositeDisposable? = null
   private var backgroundResId: Int = 0
+  private var tintResId: Int = 0
 
   init {
     if (attrs != null) {
       backgroundResId = context.resId(attrs, android.R.attr.background)
+      tintResId = context.resId(attrs, R.attr.tint)
+      if (tintResId == 0) {
+        tintResId = context.resId(attrs, android.R.attr.tint)
+      }
     }
   }
 
   override fun onAttachedToWindow() {
     super.onAttachedToWindow()
-    val obs = ViewUtil.getObservableForResId(context, backgroundResId, null)
-    if (obs != null) {
-      bgSubscription = obs
+    subs = CompositeDisposable()
+
+    val backgroundObs = getObservableForResId(context, backgroundResId, null)
+    if (backgroundObs != null) {
+      subs!! += backgroundObs
           .distinctToMainThread()
           .subscribe(
               ViewBackgroundAction(this),
               onErrorLogAndRethrow()
           )
     }
+
+    val tintObs = getObservableForResId(context, tintResId, null)
+    if (tintObs != null) {
+      subs!! += tintObs
+          .distinctToMainThread()
+          .subscribe(
+              ImageViewTintAction(this),
+              onErrorLogAndRethrow()
+          )
+    }
   }
 
   override fun onDetachedFromWindow() {
-    bgSubscription?.dispose()
+    subs?.dispose()
     super.onDetachedFromWindow()
   }
 }
