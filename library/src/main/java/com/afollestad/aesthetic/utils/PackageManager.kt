561affc060b7a19d1d41240a3d876753fffd067d
Aidan Follestad <afollestad@squareup.com>
2018-09-15 18:23:52 -0700

    Cleanup PackageManager.kt

--- library/src/main/java/com/afollestad/aesthetic/utils/PackageManager.kt
+++ library/src/main/java/com/afollestad/aesthetic/utils/PackageManager.kt
@@ -10,7 +10,6 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.AdaptiveIconDrawable
 import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
 import android.os.Build
 import android.support.annotation.RequiresApi
@@ -22,20 +21,13 @@ internal fun PackageManager.getAppIcon(packageName: String): Bitmap? {
     if (drawable is BitmapDrawable) {
       return drawable.bitmap
     } else if (drawable is AdaptiveIconDrawable) {
-      val backgroundDr = drawable.background
-      val foregroundDr = drawable.foreground
-
-      val drr = arrayOfNulls<Drawable>(2)
-      drr[0] = backgroundDr
-      drr[1] = foregroundDr
-
+      val drr = arrayOf(drawable.background, drawable.foreground)
       val layerDrawable = LayerDrawable(drr)
 
       val width = layerDrawable.intrinsicWidth
       val height = layerDrawable.intrinsicHeight
 
       val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-
       val canvas = Canvas(bitmap)
 
       layerDrawable.setBounds(0, 0, canvas.width, canvas.height)
