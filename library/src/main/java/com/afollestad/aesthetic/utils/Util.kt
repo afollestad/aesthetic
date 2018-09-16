8f310d688044f7d0dd4d94a0c441c34d2e9c9f16
Aidan Follestad <afollestad@squareup.com>
2018-09-15 18:49:08 -0700

    Cleanup Util classes more

--- library/src/main/java/com/afollestad/aesthetic/utils/Util.kt
+++ library/src/main/java/com/afollestad/aesthetic/utils/Util.kt
@@ -19,15 +19,23 @@ fun watchColor(
   with(Aesthetic.get()) {
     return when (resId) {
       0 -> fallback
-      context.resId(attr.colorPrimary) -> colorPrimary()
-      context.resId(attr.colorPrimaryDark) -> colorPrimaryDark()
+      context.resId(attr.colorPrimary),
+      context.resId(android.R.attr.colorPrimary) -> colorPrimary()
+
+      context.resId(attr.colorPrimaryDark),
+      context.resId(android.R.attr.colorPrimaryDark) -> colorPrimaryDark()
+
+      context.resId(attr.colorAccent),
+      context.resId(android.R.attr.colorAccent) -> colorAccent()
+
       context.resId(android.R.attr.statusBarColor) -> colorStatusBar()
-      context.resId(attr.colorAccent) -> colorAccent()
       context.resId(android.R.attr.windowBackground) -> colorWindowBackground()
+      
       context.resId(android.R.attr.textColorPrimary) -> textColorPrimary()
       context.resId(android.R.attr.textColorPrimaryInverse) -> textColorPrimaryInverse()
       context.resId(android.R.attr.textColorSecondary) -> textColorSecondary()
       context.resId(android.R.attr.textColorSecondaryInverse) -> textColorSecondaryInverse()
+
       else -> fallback
     }
   }
