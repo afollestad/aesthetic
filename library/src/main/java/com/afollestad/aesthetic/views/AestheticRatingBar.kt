6b6c82e26def35172d0ae0fc9fc55f26ffa3666d
iota9star <iota9star@gmail.com>
2018-10-02 12:37:35 +0800

    add ratingbar support

---
+++ library/src/main/java/com/afollestad/aesthetic/views/AestheticRatingBar.kt
@@ -0,0 +1,53 @@
+/*
+ * Licensed under Apache-2.0
+ *
+ * Designed and developed by Aidan Follestad (@afollestad)
+ */
+package com.afollestad.aesthetic.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatRatingBar
+import androidx.appcompat.widget.AppCompatSeekBar
+import com.afollestad.aesthetic.Aesthetic.Companion.get
+import com.afollestad.aesthetic.ColorIsDarkState
+import com.afollestad.aesthetic.utils.allOf
+import com.afollestad.aesthetic.utils.distinctToMainThread
+import com.afollestad.aesthetic.utils.resId
+import com.afollestad.aesthetic.utils.setTint
+import com.afollestad.aesthetic.utils.subscribeTo
+import com.afollestad.aesthetic.utils.unsubscribeOnDetach
+import com.afollestad.aesthetic.utils.watchColor
+
+/** @author Aidan Follestad (afollestad) */
+class AestheticRatingBar(
+  context: Context,
+  attrs: AttributeSet? = null
+) : AppCompatRatingBar(context, attrs) {
+
+  private var backgroundResId: Int = 0
+
+  init {
+    if (attrs != null) {
+      backgroundResId = context.resId(attrs, android.R.attr.background)
+    }
+  }
+
+  private fun invalidateColors(state: ColorIsDarkState) = setTint(state.color, state.isDark)
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+
+    allOf(
+        watchColor(
+            context,
+            backgroundResId,
+            get().colorAccent()
+        ),
+        get().isDark
+    ) { color, isDark -> ColorIsDarkState(color, isDark) }
+        .distinctToMainThread()
+        .subscribeTo(::invalidateColors)
+        .unsubscribeOnDetach(this)
+  }
+}
