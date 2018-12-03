7085680d58658327a6dc05de067047922456daa8
Aidan Follestad <afollestad@squareup.com>
2018-12-03 12:56:10 -0800

    Update license header

--- library/src/main/java/com/afollestad/aesthetic/Aesthetic.kt
+++ library/src/main/java/com/afollestad/aesthetic/Aesthetic.kt
@@ -1,7 +1,17 @@
-/*
- * Licensed under Apache-2.0
- *
+/**
  * Designed and developed by Aidan Follestad (@afollestad)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 @file:Suppress("unused")
 
@@ -64,8 +74,6 @@ import com.afollestad.aesthetic.utils.mutableArrayMap
 import com.afollestad.aesthetic.utils.plusAssign
 import com.afollestad.aesthetic.utils.save
 import com.afollestad.aesthetic.utils.setInflaterFactory
-import com.afollestad.aesthetic.utils.setLightNavBarCompat
-import com.afollestad.aesthetic.utils.setNavBarColorCompat
 import com.afollestad.aesthetic.utils.setTaskDescriptionColor
 import com.afollestad.aesthetic.utils.subscribeTo
 import com.afollestad.rxkprefs.RxkPrefs
@@ -720,12 +728,12 @@ class Aesthetic private constructor(private var context: Context?) {
           )
               .distinctToMainThread()
               .subscribe(::invalidateNavBar)
+          subs += colorWindowBackground()
+              .distinctToMainThread()
+              .subscribeTo {
+                (safeContext as? Activity)?.window?.setBackgroundDrawable(ColorDrawable(it))
+              }
         }
-        subs += colorWindowBackground()
-            .distinctToMainThread()
-            .subscribeTo {
-              (safeContext as? Activity)?.window?.setBackgroundDrawable(ColorDrawable(it))
-            }
       }
     }
 
--- spotless.license.kt
+++ spotless.license.kt
@@ -13,9 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.afollestad.aesthetic.utils
-
-import androidx.annotation.CheckResult
-
-@CheckResult
-internal fun String.isNumber() = all { it.isDigit() }
